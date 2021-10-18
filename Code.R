beb390cc28278f462a408cb5dbc4ac5ee546f460
EstebanSierraGayon <estebansierragayon@hotmail.com>
2021-10-18 15:37:12 +0800

    Added the general data and the script where it will be manipulated

---
+++ Code.R
@@ -0,0 +1,10 @@
+library(forecast)
+library(tsintermittent)
+all_data <- read.csv("C:/Users/2506/Desktop/TESIS/DOCUMENTOS TESIS/Capitulos/R/Tesis/AllData.csv", header = TRUE)
+all_data <- all_data[-c(37,38), ]
+
+cros_
+
+for (i in all_data){
+  (i)
+}
