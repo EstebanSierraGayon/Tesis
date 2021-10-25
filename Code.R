cb83a89f7f1d452d0fe9c39b547612871b13bdcd
EstebanSierraGayon <estebansierragayon@hotmail.com>
2021-10-25 15:34:08 +0800

    Added items BIP3819 and BP1998 only with Croston

--- Code.R
+++ Code.R
@@ -1,27 +1,28 @@
-install.packages("ggplot2")
+install.packages("dplyr")
+
 library(forecast)
 library(tsintermittent)
-library(ggplot2)
+library(dplyr)
+
 
 all_data <- read.csv("C:/Users/2506/Desktop/TESIS/DOCUMENTOS TESIS/Capitulos/R/Tesis/AllData.csv", header = TRUE)
+
+
+#Croston method for item BIP1271#
 bip1271 <- all_data[,1:2]
-bip1271crost <- crost(bip1271[2], h = 5)
+bip1271crost <- crost(bip1271[2], h = 5, w = 0.15, init = "naive" , outplot = 30)
 bip1271crost
 bip1271$cros_smoothed <- bip1271crost$frc.in
 
-plot.ts(bip1271$BIP001271, xlim = c(0, 40))
-lines(bip1271crost$frc.in, col="red")
-lines(bip1271crost$frc.out, col = "blue")
+#Croston method for item BIP3819#
+bip3819 <- select(all_data, X, BIP003819)
+bip3819crost <- crost(bip3819[2], h = 5, w = 0.15, outplot = 1)
+bip3819crost
+bip3819$cros_smoothed <- bip3819crost$frc.in
 
-bip1271crost
-
-
-
-qplot(X, BIP001271, data = bip1271, geom = c("point", "line"), aes(group = 1)) + 
-  geom_hline(mapping = (aes(group = 1)), bip1271$cros_smoothed)
-
-
-ggplot(bip1271, aes(x = X, y = BIP001271, group = 2))+
-  geom_line(size=1, colour = "black") 
-geom_hline
+#Croston method for item BIP1998#
+bip1998 <- select(all_data, X, BIP001998)
+bip1998crost <- crost(bip1998[2], h = 5, w = 0.15, outplot =1)
+bip1998crost
+bip1998$cros_smooth <- bip1998crost$frc.in
 
